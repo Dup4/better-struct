9800b190136db40637b36c5a21d23789ce5f25b2
Dup4 <lyuzhi.pan@gmail.com>
2022-07-12 20:05:10 +0800

    chore: rename better struct

--- test/hello_world_benchmark.cc
+++ test/hello_world_benchmark.cc
@@ -1,10 +1,7 @@
 #include <benchmark/benchmark.h>
 
-#include "better-struct/hello_world.h"
-
 static void BenchmarkHelloWorld(benchmark::State& state) {
     for (auto _ : state) {
-        HelloWorld();
     }
 }
 
