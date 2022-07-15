2c6635a0b4ea2740e7eb2926d7974f375b2d7bee
Dup4 <lyuzhi.pan@gmail.com>
2022-07-15 09:59:40 +0800

    feat: basic macros

--- include/better-struct/better-struct.h
+++ include/better-struct/better-struct.h
@@ -1,4 +1,6 @@
 #ifndef BETTER_STRUCT_BETTER_STRUCT_H
 #define BETTER_STRUCT_BETTER_STRUCT_H
 
+#include "./macros.h"  //  IWYU pragma: export
+
 #endif  // BETTER_STRUCT_BETTER_STRUCT_H
--- include/better-struct/macros.h
+++ include/better-struct/macros.h
@@ -1,4 +1,10 @@
 #ifndef BETTER_STRUCT_MACROS_H
 #define BETTER_STRUCT_MACROS_H
 
+#include "./macros_utility.h"
+
+#define BETTER_STRUCT_FIELDS(...) __BETTER_STRUCT_FIELDS(__VA_ARGS__)
+
+#define BETTER_STRUCT_DECLARE(struct, fields, ...) __BETTER_STRUCT_DECLARE(struct, fields, __VA_ARGS__)
+
 #endif  // BETTER_STRUCT_MACROS_H
